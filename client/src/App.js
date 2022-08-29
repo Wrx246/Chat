f7644da29e9313fbe59c1781da339d4b8c5674eb
Maxim Kuzmianok <jst.wrx@gmail.com>
2022-08-29 22:48:03 +0300

    init: initialize chat app

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+
+
+function App() {
+  return (
+    <div>
+      <h1>Client work</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+
+
+const app = express();
+require("dotenv").config()
+app.use(cors())
+app.use(express.json())
+
+const start = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        }).then(() => {
+            console.log('DB connected');
+        }).catch((e) => {
+            console.log(e.message);
+        })
+        app.listen(process.env.PORT, () => {
+            console.log(`Server started on ${process.env.PORT}`);
+        })
+    } catch (e) {
+        console.log(e.message);
+    }
+}
+
+start()
