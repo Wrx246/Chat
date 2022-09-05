7fd62b99aeb6269b1d3fee74200bea543aea59e3
Maxim Kuzmianok <jst.wrx@gmail.com>
2022-09-05 13:26:02 +0300

    feat: added settings bar on hover for each message

--- client/src/UI/MessageItem.jsx
+++ client/src/UI/MessageItem.jsx
@@ -2,24 +2,44 @@ import React from 'react'
 import { useState } from 'react';
 import { useEffect } from 'react';
 import ProfileImage from '../assets/images/profile-image.png'
+import Bin from '../assets/images/Bin.svg'
+import Pencil from '../assets/images/Pencil.svg'
+import Push from '../assets/images/push.svg'
+import Reply from '../assets/images/Reply.svg'
+import Smile from '../assets/images/smile.svg'
 import st from '../styles/MessageItem.module.scss'
 import { getUser } from '../utils/chatFetch';
 
-const MessageItem = ({message}) => {
+const MessageItem = ({ message }) => {
     const [friend, setFriend] = useState({})
-    const {text, sender} = message;
+    const [showSettings, setShowSettings] = useState(false)
+    const { text, sender } = message;
 
     useEffect(() => {
         getUser(sender, setFriend)
     }, [message])
 
+    const handleHover = (e) => {
+        e.preventDefault();
+        setShowSettings(!showSettings)
+    }
+
     return (
-        <div className={st.message_item}>
+        <div className={st.message_item} onMouseEnter={handleHover} onMouseLeave={handleHover}>
             <img src={ProfileImage} alt="profile img" />
             <div>
-                <span>{ friend ? friend.userName : 'Username'}</span>
+                <span>{friend ? friend.userName : 'Username'}</span>
                 <p>{text}</p>
             </div>
+            {showSettings &&
+                <div className={st.message_settings}>
+                    <img src={Smile} alt="smile" />
+                    <img src={Reply} alt="reply" />
+                    <img src={Push} alt="push" />
+                    <img src={Pencil} alt="pencil" />
+                    <img src={Bin} alt="bin" />
+                </div>
+            }
         </div>
     )
 }
